74ea80d9e20d67635109b3340961c5e6e6b105d1
peitianyu <1216582461@qq.com>
2023-10-20 18:32:36 +0800

    add: 添加img2txt模块, 用于美化代码

---
+++ src/common/img2txt.h
@@ -0,0 +1,18 @@
+#ifndef __IMG2TXT_H__
+#define __IMG2TXT_H__
+
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+
+
+
+
+
+
+
+#endif // __IMG2TXT_H__
---
+++ src/test/common/img2txt_test.cc
@@ -0,0 +1,27 @@
+#include "core/tt_test.h"
+#include "common/log.h"
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+JUST_RUN_TEST(img2txt, test)
+TEST(img2txt, test)
+{
+    cv::Mat img = cv::imread("../src/data/img.jpg");
+    cv::Mat gray;
+    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    resize(gray, gray, cv::Size(40, 20));
+    std::cout << "/*****************************************************" << std::endl;
+    std::string ascii = " .*#8";
+    for (int i = 0; i < gray.rows; i++)
+    {
+        for (int j = 0; j < gray.cols; j++){
+            int index = (int)(gray.at<uchar>(i, j) / 256.0 * 5);
+            std::cout << ascii[index];
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "*******************************************************/" << std::endl;
+}
