802eb7c4e257be14022b128a2736a23a88465834
peitianyu <1216582461@qq.com>
2023-10-22 15:34:38 +0800

    add: 添加key_board read模块

--- src/common/log.cc
+++ src/common/log.cc
@@ -5,3 +5,17 @@ std::string get_file_name(const std::string &file_name) {
     if (pos == std::string::npos) return file_name; 
     return file_name.substr(pos + 1);
 }
+
+#include <chrono>
+std::string get_curr_time()
+{
+    typedef std::chrono::system_clock Clock;
+    typedef std::chrono::milliseconds milliseconds;
+
+    auto get_time = []()->double{
+        return std::chrono::duration_cast<milliseconds>(Clock::now().time_since_epoch()).count();
+    };
+
+    static double start = get_time();
+    return "["+std::to_string(get_time()-start)+"] ";
+}
