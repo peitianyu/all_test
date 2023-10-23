d5d49b6bb9725b947517ed446f60a540ef45c338
peitianyu <1216582461@qq.com>
2023-10-23 09:06:52 +0800

    merge: 合并代码

--- src/test/common/img2txt_test.cc
+++ src/test/common/img2txt_test.cc
@@ -9,17 +9,21 @@
 JUST_RUN_TEST(img2txt, test)
 TEST(img2txt, test)
 {
-    cv::Mat img = cv::imread("../src/data/img.jpg");
+    cv::Mat img = cv::imread("../src/data/download.jpg");
     cv::Mat gray;
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
     resize(gray, gray, cv::Size(40, 20));
     std::cout << "/*****************************************************" << std::endl;
-    std::string ascii = " .*#B";
     for (int i = 0; i < gray.rows; i++)
     {
         for (int j = 0; j < gray.cols; j++){
-            int index = (int)(gray.at<uchar>(i, j) / 256.0 * 5);
-            std::cout << ascii[index];
+            uchar val = gray.at<uchar>(i, j);
+            if(val < 5) std::cout << " ";
+            else if(val < 100) std::cout << ".";
+            else if(val < 150) std::cout << "*";
+            else if(val < 200) std::cout << "#";
+            else std::cout << "█";
+            // else std::cout << "B";
         }
         std::cout << std::endl;
     }
